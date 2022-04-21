554afb4c6652d2cb46039aeade43a4ae13c0f2d8
Paul Watson <github.com@paulmwatson.com>
2022-04-21 17:05:10 +0200

    Add fullscreen option

--- src/map.ts
+++ src/map.ts
@@ -1,10 +1,12 @@
 import "./map.scss";
 import "leaflet/dist/leaflet.css";
+import "leaflet-fullscreen/dist/leaflet.fullscreen.css";
 import L from "leaflet";
 import "leaflet.gridlayer.googlemutant";
+import "leaflet-fullscreen";
 
 const LandMap = async function () {
-  const map = new L.Map("map");
+  const map = new L.Map("map", { fullscreenControl: true });  
 
   L.gridLayer
     .googleMutant({
@@ -12,9 +14,7 @@ const LandMap = async function () {
     })
     .addTo(map);
 
-  const response = await fetch(
-    "https://mapit.code4sa.org/area/9152.geojson"
-  );
+  const response = await fetch("https://mapit.code4sa.org/area/9152.geojson");
   const geojson = await response.json();
 
   const layer = L.geoJSON(geojson, {
